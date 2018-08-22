f82494fed977d1868c90fa4846c3c1f31a75c5fb
Nathan Maria <36935876+Nathanms1@users.noreply.github.com>
2018-08-21 21:56:55 -0300

    Add files via upload

---
+++ ExercicioMediaAluno.java
@@ -0,0 +1,14 @@
+package javaapplication3;
+ public class ExercicioMediaAluno {
+    public static void main(String[] args) {
+     double n1=6;
+     double n2=7;
+     double n3=8.9;    
+     double nf= (n1+n2+n3)/3;     
+        if(nf>=7){
+            System.out.println("Aluno aprovado.");
+        }else{
+            System.out.println("Aluno reprovado.");   
+     }
+   }
+ }
